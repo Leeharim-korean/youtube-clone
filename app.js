de4738ca95fb8cc8c3d6f3da917a0ebfb4ad4851
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2020-10-04 16:16:54 +0900

    #2.8 Express Core: Routing

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ import morgan from "morgan";  //
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 const app = express();        //─fist line is next gen JS and second thing is normal
-
-const PORT = 4000;
-
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
 
 function handleHome(req, res) {
     res.send("Hello from home!");
@@ -17,20 +12,16 @@ function handleHome(req, res) {
 //this is arrow function that is same thing with normal function
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-};
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: ture }));
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(betweenHome);
 
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;//It mean export all(may be) 
---
+++ router.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+export const userRouter = express.Router();//it mean export just one line
+
+userRouter.get("/", (req, res) => res.send("user index"));
+userRouter.get("/edit", (req, res) => res.send("user edit"));
+userRouter.get("/password", (req, res) => res.send("user password"));
