2ae09fb90f842df2ae64d54ce15de10664443d05
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2020-10-06 15:20:06 +0900

    #2.11 MVC Pattern
    MVC
    Model : data
    View : how does the data look
    Controller : function that looks for the data

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-import express from "express"; //┐
-import morgan from "morgan";  //
+import express from "express";
+import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
-const app = express();        //─fist line is next gen JS and second thing is normal
-
-function handleHome(req, res) {
-    res.send("Hello from home!");
-}
-//this is arrow function that is same thing with normal function
-const handleProfile = (req, res) => res.send("You are on my profile");
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: ture }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-export default app;//It mean export all(may be) 
+export default app;
