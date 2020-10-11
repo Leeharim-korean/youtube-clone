fe2dbbfd99564d040361c2b58dc94df65f757c2d
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2020-10-11 19:49:05 +0900

    #2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,19 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) => res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+    const {
+        query: { term: searchingFor }
+    } = req;
+    //const searchingFor = req.query.term;
+    res.render("search", { pageTitle: "Search", searchingFor });
+};
+
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
 //.render(Template, Object(, Object, …))
 //use render's second factor you can give Template-variable in Pug
