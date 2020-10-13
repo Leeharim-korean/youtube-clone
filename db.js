906c433c48e68aedea6c8e934c81ef7672971dbd
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2020-10-13 16:17:40 +0900

    #2.22 Home Controller part Two
    use mixin you have not to paste same source to every template

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title: 'Video awesome',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Nico",
@@ -16,7 +16,7 @@ export const videos = [
         title: 'Video aweful',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Nico",
@@ -28,7 +28,7 @@ export const videos = [
         title: 'Video something',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Nico",
