7057ef154a19d33943da8ec8c6c272c51e65d6f7
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2020-10-13 15:23:54 +0900

    #2.21 Home Controller
    make a fake database(db.js)  and use iteration

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Cloneing youtube with VanliaJS and NodeJS(feat. nomadcoder)
 - [x] Login
 - [x] Search
 - [ ] User Detail
-- [ ] Edit Profile
-- [ ] Change Password
-- [ ] Upload
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
 - [ ] Video Detail
-- [ ] Edit Video
+- [x] Edit Video
---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id: 324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Nico",
+            email: "nicp@naber.com"
+        }
+    },
+    {
+        id: 324394,
+        title: 'Video aweful',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Nico",
+            email: "nicp@naber.com"
+        }
+    },
+    {
+        id: 324395,
+        title: 'Video something',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Nico",
+            email: "nicp@naber.com"
+        }
+    }
+];
