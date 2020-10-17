2d800f8dce6b74754fdace7726e53b1122ef3b9f
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2020-10-17 14:02:16 +0900

    #3.1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,38 +1,17 @@
-export const videos = [
+import mongoose from "monggose";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/wetube",
     {
-        id: 324393,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Nico",
-            email: "nicp@naber.com"
-        }
-    },
-    {
-        id: 324394,
-        title: 'Video aweful',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Nico",
-            email: "nicp@naber.com"
-        }
-    },
-    {
-        id: 324395,
-        title: 'Video something',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Nico",
-            email: "nicp@naber.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected at DB");
+const handleError = () => console.log(`❌ Error on DB Connection : ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
