d1c13661983e67e649b6e7c7a191192b61efb293
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2020-10-26 15:55:02 +0900

    #4.1 Styles with Webpack part One

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
 const path = require("path"); // import path from "path"(same thing but webpack can understand es6 js)
+const ExtractCSS = require("extract-text-webpack-plugin");
 
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
     entry: ENTRY_FILE,
+    mode: MODE,
+    module: {
+        rules:[
+            {
+                test: /\.(scss)$/,
+                use: ExtractCSS.extract([
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader",
+                    },
+                    {
+                        loader: "scss-loader",
+                    },
+                ]),
+            },
+        ],
+    },
     output: {
         path: OUTPUT_DIR,
         filename: "[name].[format]",
